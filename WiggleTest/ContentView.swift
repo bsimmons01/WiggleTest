c3c39212b7d766311f16f19f6becc8c36b15689b
Brian Simmons <bsimmons01@gmail.com>
2025-04-04 08:34:10 -0700

    Project Test started

--- WiggleTest/ContentView.swift
+++ WiggleTest/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .font(.title)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
     }
