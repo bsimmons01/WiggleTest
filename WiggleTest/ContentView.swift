d9e1510b09cc5736f2f716b6cb92674515151641
Brian Simmons <bsimmons01@gmail.com>
2025-04-04 08:47:24 -0700

    Wiggling code

--- WiggleTest/ContentView.swift
+++ WiggleTest/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animateViewIn = false
+    @State private var hintWiggle = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .font(.title)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+                
+                VStack {
+                    if animateViewIn {
+                        Image(systemName: "questionmark.app.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100)
+                            .foregroundColor(.green)
+                            .rotationEffect(.degrees(hintWiggle ? -13 : -17))
+                            .padding()
+                            .transition(.offset(x: -geo.size.width / 2 ))
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 0.1).repeatCount(9).delay(5).repeatForever()) {
+                                    hintWiggle = true
+                                }
+                            }
+                    }
+                }
+                .animation(.easeOut(duration: 1.5).delay(2), value: animateViewIn)
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .ignoresSafeArea()
+        .onAppear {
+            animateViewIn = true
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    VStack {
+        ContentView()
+    }
 }
