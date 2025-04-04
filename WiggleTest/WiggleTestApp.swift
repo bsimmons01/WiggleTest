4d7f84ed97bf3dc700b97f6192b10af50f7fce2b
Brian Simmons <bsimmons01@gmail.com>
2025-04-04 08:32:19 -0700

    Initial Commit

---
+++ WiggleTest/WiggleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiggleTestApp.swift
+//  WiggleTest
+//
+//  Created by Brian Simmons on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WiggleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
